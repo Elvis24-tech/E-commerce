ed3f3391272ff80dc08057012b0650eac71b0682
elvis <kariuki.elvis@student.moringaschool.com>
2025-07-10 19:44:10 +0300

    change

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa';
+import { useCart } from '../context/CartContext';
+
+const Header = () => {
+  const { getCartItemCount } = useCart(); 
+  
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header-content">
+          <Link to="/" className="logo">
+            ReactShop
+          </Link>
+          <nav className="nav">
+            <Link to="/">Home</Link>
+            <Link to="/add-product">Sell</Link> 
+            <Link to="/cart" className="cart-link">
+              <FaShoppingCart />
+              <span className="cart-count">{getCartItemCount()}</span>
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
